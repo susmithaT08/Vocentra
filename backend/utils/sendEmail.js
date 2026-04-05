b8eda996d0e02588e22b9168c7535e15cb21a6ee
Susmitha0820 <haribro0211@gmail.com>
2026-04-05 15:44:34 +0530

    fix smtp email

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,10 +2,21 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, subject, html) => {
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
+        }
+    });
+
+    // Check connection
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log("EMAIL ERROR:", error);
+        } else {
+            console.log("EMAIL READY");
         }
     });
 
